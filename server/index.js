0489ee3c9ed992a03622415e2867dbea7b72d15c
Hagen Strahl <hagen.strahl@gmail.com>
2021-02-24 10:29:58 +0100

    add cors package

--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
-const glob = require("glob");
 const fs = require("fs");
+const cors = require("cors");
 
 // BODY-PARSER
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
 // Local server
 const port = 8000;
