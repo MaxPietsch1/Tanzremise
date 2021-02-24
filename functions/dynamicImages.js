c470f47db6f051437388c82b8e6ae37a08be6a13
Hagen Strahl <hagen.strahl@gmail.com>
2021-02-24 21:37:51 +0100

    fix netlify support locally

--- functions/dynamicImages.js
+++ functions/dynamicImages.js
@@ -1,3 +1,4 @@
+"use strict";
 const express = require("express");
 const serverless = require("serverless-http");
 const app = express();
@@ -5,37 +6,37 @@ const fs = require("fs");
 const bodyParser = require("body-parser");
 
 // Local server
-const port = 8000;
-app.listen(port, () => {
-  console.log(`Running on localhost: ${port}`);
-});
-app.use(express.static("public"));
+// const port = 8000;
+// app.listen(port, () => {
+//   console.log(`Running on localhost: ${port}`);
+// });
+// app.use(express.static("public"));
 
 // Setup server for Netlify
 const router = express.Router();
-router.get("/", (req, res) => {
-  res.writeHead(200, { "Content-Type:": "text/html" });
-  res.write("<h1>Hello from Express server</h1>");
-  res.end();
-});
-router.get("/another", (req, res) => res.json({ route: req.originalUrl }));
-router.post("/", (req, res) => res.json({ postBody: req.body }));
+// router.get("/", (req, res) => {
+//   res.writeHead(200, { "Content-Type:": "text/html" });
+//   res.write("<h1>Hello from Express server</h1>");
+//   res.end();
+// });
+router.get("/", (req, res) => res.send(imagesEndpoint));
+// router.post("/", (req, res) => res.json({ postBody: req.body }));
 
 // BODY-PARSER
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use("/.netlify/functions/server", router);
-app.use("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "../public/index.html"))
-);
+app.use("/.netlify/functions/dynamicImages", router);
+// app.use("/", (req, res) =>
+//   res.sendFile(path.join(__dirname, "../public/index.html"))
+// );
 
 // Dynamic Image Endpoint
 const imagesEndpoint = [];
 
 // GET image names from imagesEndpoint
-app.get("/dynamicImages", (req, res) => {
-  res.send(imagesEndpoint);
-});
+// app.get("/dynamicImages", (req, res) => {
+//   res.send(imagesEndpoint);
+// });
 
 function buildImagesEndpoint() {
   const src = "public/images/gallery";
--- public/javascript/gallerie.js
+++ public/javascript/gallerie.js
@@ -125,7 +125,7 @@ function buildNavigationElement(showObjects, isAll = false) {
 }
 
 async function dynamicImages() {
-  const response = await fetch("/.netlify/functions/server/dynamicImages");
+  const response = await fetch("/.netlify/functions/dynamicImages");
   const data = await response.json();
 
   const lazyloadWrapper = document.querySelector(".lazyload-wrapper");
