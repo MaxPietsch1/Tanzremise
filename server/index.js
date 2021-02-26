f2017eabefe7bcd29320de2fa5618432a69682fc
Max Pietsch <max.pietsch@hotmail.com>
2021-02-26 13:26:24 +0100

    Update img and position of elements, comment code in server

--- server/index.js
+++ server/index.js
@@ -12,19 +12,20 @@ app.listen(port, () => {
 app.use(express.static("public"));
 
 // Setup server for Netlify
-const router = express.Router();
-router.get("/", (req, res) => {
-  res.writeHead(200, { "Content-Type:": "text/html" });
-  res.write("<h1>Hello from Express server</h1>");
-  res.end();
-});
-router.get("/another", (req, res) => res.json({ route: req.originalUrl }));
-router.post("/", (req, res) => res.json({ postBody: req.body }));
+// const router = express.Router();
+// router.get("/", (req, res) => {
+//   res.writeHead(200, { "Content-Type:": "text/html" });
+//   res.write("<h1>Hello from Express server</h1>");
+//   res.end();
+// });
+// router.get("/another", (req, res) => res.json({ route: req.originalUrl }));
+// router.post("/", (req, res) => res.json({ postBody: req.body }));
 
 // BODY-PARSER
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use("/.netlify/functions/server", router);
+// app.use("/.netlify/functions/server", router);
+
 // If this is here, then Images in Gallery doesnt work
 // app.use("/", (req, res) =>
 //   res.sendFile(path.join(__dirname, "../public/index.html"))
