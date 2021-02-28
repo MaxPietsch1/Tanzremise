990e0c3660d0c2d2b92ed4ef2a1c40c012ee4445
Max Pietsch <max.pietsch@hotmail.com>
2021-02-28 11:38:15 +0100

    fix issues

--- build-script/dynamicImages.js
+++ build-script/dynamicImages.js
@@ -5,9 +5,11 @@ const src = "public/images/gallery";
 
 const years = fs.readdirSync(src).filter((d) => d !== ".DS_Store");
 years.forEach((year) => {
-  let shows = fs.readdirSync(src + "/" + year);
+  let shows = fs.readdirSync(src + "/" + year).filter((d) => d !== ".DS_Store");
   shows.forEach((show) => {
-    let images = fs.readdirSync(src + "/" + year + "/" + show);
+    let images = fs
+      .readdirSync(src + "/" + year + "/" + show)
+      .filter((d) => d !== ".DS_Store");
 
     const showObj = {
       year: year,
